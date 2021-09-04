b8668500398b3d6084b6130be3f102904fa59fce
jeziellecunha <jezielle.cunha@gmail.com>
2021-09-04 15:55:17 -0300

    alteração de ajuste no código routes

--- src/routes/abrigo.routes.js
+++ src/routes/abrigo.routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/abrigoController");
 
+router.get("/oi", (req, resp)=>{
+    resp.status(200).send({"mensagem":"oi to aqui ta funcionando "})
+})
+
+
 router.get("/", controller.getAll)
 router.get("/:id", controller.getById)
 
