7f0b8a541ecbc929e51b4c2fc7cf727483352e3c
jeziellecunha <jezielle.cunha@gmail.com>
2021-09-04 13:58:21 -0300

    conexão com heroko e nuvens, alteração no código

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const db = require("./src/data/database")
 db.connect()
